3ea8bcc327c2b7ede1dff14db76b1944fc976528
David Leininger <davidleininger@hey.com>
2021-08-04 23:18:57 -0500

    Selecting

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,45 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+      </NativeSelect>
+      <Presentation>{displayedValue} <IconWrapper id="chevron-down" strokeWidth="2" size="24" /></Presentation>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+const NativeSelect = styled.select`
+  height: 100%;
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
+const Presentation = styled.div`
+  align-items: center;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  display: flex;
+  padding: 12px 16px;
+  /* adjust padding right for icon */
+  padding-right: 10px;
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+  ${NativeSelect}:focus + & {
+    box-shadow: 0 0 0 2px ${COLORS.primary};
+  }
+`;
+const IconWrapper = styled(Icon)`
+  margin-left: 18px;
+`
 
 export default Select;
